3952ea458a90198cddc889ed03cabc140af7e265
Asım <asimzorlu@gmail.com>
2024-05-21 20:42:08 +0300

    Update init.py

--- init.py
+++ init.py
@@ -1,19 +1,19 @@
+from selenium import webdriver
 from time import sleep
+import warnings
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from random import randint, sample, choice
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.window import WindowTypes
-from seleniumbase import Driver
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import undetected_chromedriver as uc
 
 class Setup:
     def init(self):
-        self.browser = Driver(headless=True)
+        options = uc.ChromeOptions()
+        options.add_argument('--headless')
+        self.browser = uc.Chrome(options=options)
 
     def close_browser(self):
-        self.browser.delete_all_cookies()
         self.browser.quit()
